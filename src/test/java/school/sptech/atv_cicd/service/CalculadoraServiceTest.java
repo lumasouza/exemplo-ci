595c8974995a7f2e6939cc9e5b0b2d5f1d853824
Luma Souza <luma.souza@sptech.school>
2024-11-14 20:38:29 -0300

    gerar exception em divisao com 0

--- src/test/java/school/sptech/atv_cicd/service/CalculadoraServiceTest.java
+++ src/test/java/school/sptech/atv_cicd/service/CalculadoraServiceTest.java
@@ -2,8 +2,11 @@ package school.sptech.atv_cicd.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculadoraServiceTest {
 
@@ -21,5 +24,25 @@ public class CalculadoraServiceTest {
 
         // Verificação
         assertEquals(esperado, resultado);
+    }
+
+    @Test
+    @DisplayName("Quando acionado com 10 e 0, então deve lançar uma exceção")
+    public void testDividirPorZero() {
+        // Cenário
+        CalculadoraService calculadoraService = new CalculadoraService();
+        Double a = 10.0;
+        Double b = 0.0;
+        var expectedMessage = "400 BAD REQUEST \"Não é possível dividir por zero\"";
+
+        // Ação
+        ResponseStatusException exception = assertThrows(
+            ResponseStatusException.class,
+            () -> calculadoraService.dividir(a, b)
+        );
+
+
+        // Verificação
+        assertEquals(expectedMessage, exception.getMessage());
     }
 }
